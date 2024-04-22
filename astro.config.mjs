eac3d1362db89f5fa0309ebc39b2af16472002ef
jcastellanosierras <jcastellanosierras@gmail.com>
2024-04-22 16:15:45 +0200

    feat: add node as adapter and auth-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import auth from "auth-astro";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [auth(), db()]
+});
