01652e68a05ce68a82a9f24bbf73063d6ab2a761
jcastellanosierras <jcastellanosierras@gmail.com>
2024-04-25 22:10:20 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import auth from "auth-astro";
 import db from "@astrojs/db";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   integrations: [auth(), db(), react()]
 });
